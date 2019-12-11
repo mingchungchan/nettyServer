03f27096e0d552f5010cc413150c1f0855e927d2
mingchung <mingchungchan@163.com>
2019-12-11 14:15:44 +0800

    初始化

---
+++ src/main/java/myNetty/protocol/RpcDecoder.java
@@ -0,0 +1,36 @@
+package myNetty.protocol;
+
+import com.alibaba.fastjson.JSON;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class RpcDecoder extends ByteToMessageDecoder {
+
+    //目标对象类型进行解码
+    private Class<?> target;
+
+    public RpcDecoder(Class target) {
+        this.target = target;
+    }
+
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
+        if (byteBuf.readableBytes() < 4) { //不够长度丢弃
+            return;
+        }
+        byteBuf.markReaderIndex(); //标记一下当前的readIndex的位置
+        int dataLength = byteBuf.readInt(); // 读取传送过来的消息的长度。ByteBuf 的readInt()方法会让他的readIndex增加4
+
+        if (byteBuf.readableBytes() < dataLength) { //读到的消息体长度如果小于我们传送过来的消息长度，则resetReaderIndex. 这个配合markReaderIndex使用的。把readIndex重置到mark的地方
+            byteBuf.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        byteBuf.readBytes(data);
+
+        Object obj = JSON.parseObject(data, target); //将byte数据转化为我们需要的对象
+        list.add(obj);
+    }
+}
---
+++ src/main/java/myNetty/server/ServerHandler.java
@@ -0,0 +1,73 @@
+package myNetty.server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * 用来实现Server端接收和处理消息的逻辑
+ */
+public class ServerHandler extends ChannelHandlerAdapter {
+
+    //接受client发送的消息
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf buf = (ByteBuf) msg;
+        String recieved = getMessage(buf);
+        System.err.println("服务器接收到客户端消息：" + recieved);
+
+        try {
+            ctx.writeAndFlush(getSendByteBuf("你好，客户端"));
+            System.err.println("服务器回复消息：你好，客户端");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    /**
+     *
+     * 从ByteBuf中获取信息 使用UTF-8编码返回
+     */
+    private String getMessage(ByteBuf buf) {
+        byte[] con = new byte[buf.readableBytes()];
+        buf.readBytes(con);
+        try {
+            return new String(con, "UTF8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private ByteBuf getSendByteBuf(String message) throws UnsupportedEncodingException {
+        byte[] req = message.getBytes("UTF-8");
+        ByteBuf pingMessage = Unpooled.buffer();
+        pingMessage.writeBytes(req);
+
+        return pingMessage;
+    }
+
+    //通知处理器最后的channelRead()是当前批处理中的最后一条消息时调用
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("服务端接收数据完毕..");
+        ctx.flush();
+    }
+
+    //读操作时捕获到异常时调用
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+
+    //客户端去和服务端连接成功时触发
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//        ctx.writeAndFlush("hello client");
+    }
+}
